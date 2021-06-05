0b383c1a40f0e371875a6d53f397b80bd26bddd7
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-06-05 20:55:31 +0200

    Addding Main Java File

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+//SOLID Principles
+ //S stands for Single Responsibility Model
+ //O stands for Open Closed Priniciple
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("Hello World From Visual Studio Code");
+    }
+}
